9165221b2def39e6310c772235a45ed5da25b6a2
nemanjaASE <nightwalker44ASE@gmail.com>
2024-08-20 11:40:01 +0200

    Add reCAPTCHA v2

--- secure-app/app/forms/login_form.py
+++ secure-app/app/forms/login_form.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
+    recaptcha = RecaptchaField()
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=32)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
